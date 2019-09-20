cbb1fe69450341eeb4db869d34be6df909f838b1
Sharanya <sharuv89@gmail.com>
2019-09-20 17:54:23 +0530

    Adding React and styled packages plus testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: red;
+  height: 200px;
+  width: 200px;
+`;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <Container className="App"></Container>;
+};
 
 export default App;
