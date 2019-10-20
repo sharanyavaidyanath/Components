d95a49f073e2907c39d1d284069218c6e8242282
Sharanya <sharuv89@gmail.com>
2019-10-20 09:59:22 +0530

    video highlight component added

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import styled from "styled-components";
 import CardComponent from "./card";
 import FlipCard from "./flipcard";
 import TorchEffect from "./torcheffect";
+import VideoHighlight from "./video-highlight";
+import { VIDEO_HIGHLIGHT_LIST } from "./utils/constants";
 
 const Container = styled.div`
   margin: 0;
   padding: 0;
-  min-height: 100vh;
   background: #333;
   display: flex;
   justify-content: center;
@@ -16,10 +17,23 @@ const Container = styled.div`
   font-family: "Courier New", Courier, monospace;
 `;
 
+const ContainerWithHorizontalSpace = styled.div`
+  display: grid;
+  row-gap: 80px;
+`;
+
 const App: React.FC = () => {
   return (
     <Container className="App">
-      <TorchEffect />
+      <ContainerWithHorizontalSpace>
+        {VIDEO_HIGHLIGHT_LIST.map((element, index) => (
+          <VideoHighlight
+            {...element}
+            key={index}
+            videoOnLeft={index % 2 === 1}
+          />
+        ))}
+      </ContainerWithHorizontalSpace>
     </Container>
   );
 };
---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+import dexter from "../videos/DextersLab.mp4";
+import tom from "../videos/Tomandjerry.mp4";
+
+export const VIDEO_HIGHLIGHT_LIST = [
+  { highlight: "Dexters Lab", video: dexter },
+  { highlight: "Tom and Jerry", video: tom },
+];
