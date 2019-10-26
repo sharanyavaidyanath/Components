3dd2fdba8c90fe432168e1563990a5058fb35176
Sharanya <sharuv89@gmail.com>
2019-10-26 11:44:09 +0530

    Added plan duration and plan data in constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,3 +9,80 @@ export const VIDEO_HIGHLIGHT_LIST = [
   { highlight: "Make way for Noddy", video: noddy },
   { highlight: "Powerpuff Girls", video: powerpuff },
 ];
+
+export const PLANS_DATA = [
+  {
+    planName: "Solo",
+    monthlyPrice: 45,
+    yearlyPrice: 460,
+    features: [
+      "License for 1 device",
+      "Windows, Linux, macOS",
+      "All features",
+      "All future updates",
+      "Email support",
+    ],
+  },
+  {
+    planName: "Trio",
+    monthlyPrice: 125,
+    yearlyPrice: 1060,
+    features: [
+      "License for 3 devices",
+      "Windows, Linux, macOS",
+      "All features",
+      "All future updates",
+      "Email support",
+      "License management",
+    ],
+  },
+  {
+    planName: "Team",
+    monthlyPrice: 49,
+    yearlyPrice: 500,
+    features: [
+      "Up to 10 devices",
+      "Windows, Linux, macOS",
+      "All features",
+      "All future updates",
+      "Email support",
+      "License management",
+      "Priority support",
+      "Priority feature requests",
+    ],
+  },
+  {
+    planName: "Work",
+    monthlyPrice: 49,
+    yearlyPrice: 500,
+    features: [
+      "Up to 10 devices",
+      "Windows, Linux, macOS",
+      "All features",
+      "All future updates",
+      "Email support",
+      "License management",
+      "Priority support",
+      "Priority feature requests",
+    ],
+  },
+];
+
+export const PLAN_DURATIONS = {
+  MONTHLY: "Monthly",
+  YEARLY: "Annualy",
+};
+
+export const FONT_SIZES = {
+  SMALLER: "12px",
+  SMALL: "14px",
+  NORMAL: "16px",
+  BIG: "18px",
+  BIGGER: "20px",
+  BIGGEST: "24px",
+  LARGE: "30px",
+  LARGER: "36px",
+  LARGEST: "48px",
+  GREAT: "60px",
+  GREATEST: "72px",
+};
