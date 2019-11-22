7a0a38c2092205df0816395f6e11b809e6125141
Sharanya <sharuv89@gmail.com>
2019-11-22 22:46:16 +0530

    Added social media butons and title

--- src/profile-page/index.tsx
+++ src/profile-page/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import profileLogo from "../images/SVS.png";
-import { href } from "../utils/constants";
+import twitter from "../images/twitter.png";
+import facebook from "../images/facebook.png";
+import linkedIn from "../images/linkedin.png";
 
 const OuterContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #333;
+  overflow: hidden;
 `;
 const CenterBorder = styled.div`
   display: block;
@@ -19,6 +22,16 @@ const CenterBorder = styled.div`
   color: #333;
   text-align: center;
   font: 700 30px/120px "Quicksand", sans-serif;
+  animation: rotateOne 4s;
+  @keyframes rotateOne {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+
   &&::before {
     display: block;
     width: 120px;
@@ -45,37 +58,107 @@ const CenterBorder = styled.div`
   }
 `;
 
-const StyledSvg = styled.svg`
+const StyledImg = styled.img`
   width: 80%;
+  margin-top: 30px;
 `;
+
+const StyledLinkOne = styled.a`
+  &&:hover {
+    cursor: pointer;
+  }
+`;
+const LogoContainer = styled.div`
+  width: 150px;
+  height: 150px;
+  margin: auto;
+`;
+const Name = styled.div`
+  text-align: center;
+  position: relative;
+  top: 45%;
+  transform: translateY(-50%);
+  margin: 0 auto;
+  width: 320px;
+  &&:hover .shape {
+    stroke-width: 2px;
+    stroke-dashoffset: 0;
+    stroke-dasharray: 760;
+  }
+`;
+
+const Rect = styled.rect`
+  stroke-dasharray: 140 540;
+  stroke-dashoffset: -474;
+  stroke-width: 8px;
+  fill: transparent;
+  stroke: #dfcdc3;
+  border-bottom: 5px solid black;
+  transition: stroke-width 1s, stroke-dashoffset 1s, stroke-dasharray 1s;
+`;
+
+const Text = styled.div`
+  font-family: "Roboto Condensed";
+  position: relative;
+  top: 45px;
+  font-size: 22px;
+  line-height: 32px;
+  letter-spacing: 8px;
+  color: #719192;
+`;
+const SocialMedia = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 45px);
+  margin-left: 45%;
+  margin-top: 180px;
+  grid-column-gap: 10px;
+`;
+const Twitter = styled.img`
+  height: 25px;
+`;
+const Facebook = styled.img`
+  height: 25px;
+`;
+const LinkedIn = styled.img`
+  height: 25px;
+`;
+const TwitterLink = styled.a`
+  cursor: pointer;
+`;
+const FacebookLink = styled.a`
+  cursor: pointer;
+`;
+const LinkedInLink = styled.a`
+  cursor: pointer;
+`;
+
 const ProfilePage = () => {
   return (
     <OuterContainer>
-      <CenterBorder>
-        <StyledSvg
-          width="546px"
-          height="281px"
-          viewBox="0 0 546 281"
-          version="1.1"
-        >
-          <g
-            id="Page-1"
-            stroke="none"
-            strokeWidth="1"
-            fill="none"
-            fillRule="evenodd"
-          >
-            <image
-              id="Bitmap"
-              x="0"
-              y="0"
-              width="546"
-              height="281"
-              href={href}
-            ></image>
-          </g>
-        </StyledSvg>
-      </CenterBorder>
+      <Name>
+        <Text className="text">SHARANYA</Text>
+        <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
+          <Rect className="shape" height="60" width="320" />
+        </svg>
+      </Name>
+      <LogoContainer>
+        <CenterBorder>
+          <StyledLinkOne>
+            <StyledImg src={profileLogo}></StyledImg>
+          </StyledLinkOne>
+        </CenterBorder>
+      </LogoContainer>
+      <SocialMedia>
+        <TwitterLink href="https://twitter.com" target="_blank">
+          <Twitter src={twitter}></Twitter>
+        </TwitterLink>
+        <FacebookLink href="https://facebook.com" target="_blank">
+          <Facebook src={facebook}></Facebook>
+        </FacebookLink>
+        <LinkedInLink href="https://linkedin.com" target="_blank">
+          <LinkedIn src={linkedIn}></LinkedIn>
+        </LinkedInLink>
+      </SocialMedia>
     </OuterContainer>
   );
 };
